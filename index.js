93d28201433e67f35373c0c5ff3b691488d00a4b
Pedro Otávio <pedrootavio.bezerra14@gmail.com>
2024-05-24 14:33:52 -0300

    utilização de comentários ao decorrer do código js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+// Atributos importantes para a estilização css
 const main = document.querySelector("main")
 const root = document.querySelector(":root")
+// Inputs de cálculo e resultado
 const input = document.getElementById("input")
 const resultInput = document.getElementById("result")
+// Arrays que conterá todas as teclas disponíveis para cálculos
 const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
 
+// para cada tecla, enviar o valor ao input da calculadora
 document.querySelectorAll(".charKey").forEach(function (charKeyBtn) {
   charKeyBtn.addEventListener("click", function () {
     const value = charKeyBtn.dataset.value
@@ -11,11 +15,13 @@ document.querySelectorAll(".charKey").forEach(function (charKeyBtn) {
   })
 })
 
+// Evento 'clear'
 document.getElementById("clear").addEventListener("click", function () {
   input.value = ""
   input.focus()
 })
 
+// Funcionalidade de uso do teclado / backspace / enter
 input.addEventListener("keydown", function (ev) {
   ev.preventDefault()
   if (allowedKeys.includes(ev.key)) {
@@ -32,6 +38,7 @@ input.addEventListener("keydown", function (ev) {
 
 document.getElementById("equal").addEventListener("click", calculate)
 
+// Execução de código javascript (eval) e estilização de error
 function calculate() {
   resultInput.value = "ERROR"
   resultInput.classList.add("error")
@@ -40,6 +47,7 @@ function calculate() {
   resultInput.classList.remove("error")
 }
 
+// Copiar o resultado para área de transferência
 document.getElementById("copyToClipboard").addEventListener("click", function (ev) {
   const button = ev.currentTarget
   if (button.innerText === "Copy") {
